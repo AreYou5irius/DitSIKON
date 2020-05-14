e7727c32336457c370c69a5187e72d126cee844d
Stefan de Haas Hansen <stef9239@edu.easj.dk>
2020-05-14 13:59:51 +0200

    Navigation fra menu er gjort. Kort og Info views er opretter. Info er "done" udover design. Kort har en dropdown button der skal "virke"

--- SIKONClient/Info.xaml.cs
+++ SIKONClient/Info.xaml.cs
@@ -13,35 +13,18 @@ using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 
-// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
 
 namespace SIKONClient
 {
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class MainPage : Page
+    public sealed partial class Info : Page
     {
-        public MainPage()
+        public Info()
         {
-
             this.InitializeComponent();
-            Frame.Navigate((typeof(Home)));
-        }
-
-        private void Kurser_Click(object sender, RoutedEventArgs e)
-        {
-            Frame.Navigate(typeof(Kurser));
-        }
-
-        private void Kort_OnClick(object sender, RoutedEventArgs e)
-        {
-            Frame.Navigate(typeof(Kort));
-        }
-
-        private void Info_OnClick(object sender, RoutedEventArgs e)
-        {
-            Frame.Navigate(typeof(Info));
         }
     }
 }
