c3efe7571d6d83e7c9d0c205ed62d76694d792e7
Stefan de Haas Hansen <stef9239@edu.easj.dk>
2020-05-20 11:15:10 +0200

    Test

---
+++ SIKONClassLibrary/EventHandlers/AcccountToEventHandler.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SIKONClassLibrary.EventHandlers
+{
+    public class AcccountToEventHandler : IHandle<AccountToEvent>
+    {
+        private const string ServerUrl = "http://localhost:53683/";
+        private const string RequestUri = "api/AccountToEvents";
+
+        public void Create(AccountToEvent Obj)
+        {
+            HttpClientHandler clientHandler = new HttpClientHandler();
+            clientHandler.UseDefaultCredentials = true;
+            using (var client = new HttpClient(clientHandler))
+            {
+                client.BaseAddress = new Uri(ServerUrl);
+                client.DefaultRequestHeaders.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                try
+                {
+                    var response = client.PostAsJsonAsync($"{RequestUri}", Obj).Result;
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e);
+                    throw;
+                }
+            }
+            
+        }
+
+        public List<AccountToEvent> Read()
+        {
+            HttpClientHandler clientHandler = new HttpClientHandler();
+            clientHandler.UseDefaultCredentials = true;
+            List<AccountToEvent> AccountToEventList = new List<AccountToEvent>();
+            using (var client = new HttpClient(clientHandler))
+            {
+                client.BaseAddress = new Uri(ServerUrl);
+                client.DefaultRequestHeaders.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                try
+                {
+                    var response = client.GetAsync($"{RequestUri}").Result;
+                    if (response.IsSuccessStatusCode)
+                    {
+                        var List = response.Content.ReadAsAsync<IEnumerable<AccountToEvent>>().Result;
+                        foreach (var ate in List)
+                        {
+                            AccountToEventList.Add(ate);
+                        }
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e);
+                    throw;
+                }
+            }
+            return AccountToEventList;
+        }
+
+        public AccountToEvent ReadFrom(int ID)
+        {
+            HttpClientHandler clientHandler = new HttpClientHandler();
+            clientHandler.UseDefaultCredentials = true;
+            using (var client = new HttpClient(clientHandler))
+            {
+                client.BaseAddress = new Uri(ServerUrl);
+                client.DefaultRequestHeaders.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                try
+                {
+                    AccountToEvent ev = null;
+                    var response = client.GetAsync($"{RequestUri}/{ID}").Result;
+                    if (response.IsSuccessStatusCode)
+                    {
+                        ev = response.Content.ReadAsAsync<AccountToEvent>().Result;
+                    }
+                    return ev;
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e);
+                    throw;
+                }
+            }
+        }
+
+        public AccountToEvent ReadFrom(string ID)
+        {
+            HttpClientHandler clientHandler = new HttpClientHandler();
+            clientHandler.UseDefaultCredentials = true;
+            using (var client = new HttpClient(clientHandler))
+            {
+                client.BaseAddress = new Uri(ServerUrl);
+                client.DefaultRequestHeaders.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                try
+                {
+                    AccountToEvent ev = null;
+                    var response = client.GetAsync($"{RequestUri}/{ID}").Result;
+                    if (response.IsSuccessStatusCode)
+                    {
+                        ev = response.Content.ReadAsAsync<AccountToEvent>().Result;
+                    }
+                    return ev;
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e);
+                    throw;
+                }
+            }
+        }
+
+        public bool Update(AccountToEvent Obj, int ID)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Update(AccountToEvent Obj, string ID)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(int ID)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(string ID)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ SIKONClient/ViewModelUserPage.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SIKONClassLibrary;
+using SIKONClient.Model;
+
+
+namespace SIKONClient
+{
+    class ViewModelUserPage
+    {
+        public Singleton SikonSingleton { get; set; }
+
+        public AccountToEvent AccountToEvent { get; set; }
+
+        public ObservableCollection<AccountToEvent> MyEventsList { get; set; }
+
+        public ViewModelUserPage()
+        {
+
+            SikonSingleton = Singleton.Instance;
+
+            List<AccountToEvent> liste = new List<AccountToEvent>();
+
+            MyEventsList = new ObservableCollection<AccountToEvent>();
+            foreach (var a in liste.Where(a => a.Account_ID == SikonSingleton.LoggedAccount.Email))
+            {
+                MyEventsList.Add(a);
+            }
+
+            MyEventsList = new ObservableCollection<AccountToEvent>();
+
+        }
+
+
+
+    }
+}
