de11fa7ba9d494ef9a773f336714e38fdaf4d66e
Maja Seehusen Bloch <maja8202@edu.easj.dk>
2020-05-20 13:56:58 +0200

    Har tilføjet en AccountToEventHandler
    Har lavet AddEventToAccount funktion og AddQuestionToEvent funktion
    
    lavet binding på Tilmeld og Send (KursusSiden XAML)

--- SIKONClient/ViewModelKursusSide.cs
+++ SIKONClient/ViewModelKursusSide.cs
@@ -7,6 +7,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Input;
 using Windows.ApplicationModel.Calls;
+using Eventmaker.Common;
 using SIKONClassLibrary;
 using SIKONClassLibrary.EventHandlers;
 using SIKONClient.Model;
@@ -15,32 +16,59 @@ namespace SIKONClient
 {
     class ViewModelKursusSide
     {
-       // private ICommand  _tilmeldCommand;
+
+        private ICommand _tilmeldCommand;
+        private ICommand _sendCommand;
 
         public Singleton SikonSingleton { get; set; }
 
         public ObservableCollection<Question> QuestionList { get; set; } // vi opretter en reference til vores OC af question
-        
+
         public Question UserQuestion { get; set; }
 
-        //public ICommand TilmeldCommand { get; set; }  //Hertil er jeg nået 19. maj MAJA lav Icommand færdig
+        public ICommand TilmeldCommand { get; set; }
+
+        public ICommand SendCommand { get; set; }
 
         public ViewModelKursusSide()
         {
-        
-           SikonSingleton = Singleton.Instance;
 
-           List<Question> liste = new QuestionHandler().Read();
+            TilmeldCommand = new RelayCommand(AddEventToAccount);
 
-           QuestionList = new ObservableCollection<Question>();
-           foreach (var q in liste.Where(q => q.Event_ID == SikonSingleton.SelectedEvent.ID))
-           { 
-               QuestionList.Add(q);
-           }
+            SendCommand = new RelayCommand(AddQuestionToEvent);
 
-           UserQuestion = new Question();
-            
+            SikonSingleton = Singleton.Instance;
+
+
+
         }
 
+        private void AddEventToAccount()
+        {
+            List<Event> Eventliste = new EventsHandler().Read();
+            foreach (var e in Eventliste.Where(e => e.ID == SikonSingleton.SelectedEvent.ID))
+            {
+                if (SikonSingleton != null)
+                {
+                    SikonSingleton.LoggedAccount.Event.Add(e);
+                }
+
+            }
+
+        }
+
+        private void AddQuestionToEvent()
+        {
+            List<Question> liste = new QuestionHandler().Read();
+
+            QuestionList = new ObservableCollection<Question>();
+            foreach (var q in liste.Where(q => q.Event_ID == SikonSingleton.SelectedEvent.ID))
+            {
+                QuestionList.Add(q);
+            }
+
+            UserQuestion = new Question();
+
+        }
     }
 }
