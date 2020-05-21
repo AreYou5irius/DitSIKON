bb05e0798b39af49211075cd8d2c6132c18c87ca
Stefan de Haas Hansen <stef9239@edu.easj.dk>
2020-05-21 13:18:59 +0200

    UserPage now shows the events the user is "attending". It is now possible to change password and (not email) from userpage. More info on the info page.

--- SIKONClient/ViewModelUserPage.cs
+++ SIKONClient/ViewModelUserPage.cs
@@ -4,8 +4,11 @@ using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
+using Windows.ApplicationModel.Contacts.DataProvider;
 using SIKONClassLibrary;
 using SIKONClassLibrary.EventHandlers;
+using SIKONClient.Common;
 using SIKONClient.Model;
 
 
@@ -13,30 +16,63 @@ namespace SIKONClient
 {
     public class ViewModelUserPage
     {
+        public ICommand UpdateUserInfoCommand { get; set; }
+
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+
         public Singleton SikonSingleton { get; set; }
 
         public AccountToEvent AccountToEvent { get; set; }
 
-        public ObservableCollection<AccountToEvent> MyEventsList { get; set; }
+        public ObservableCollection<Event> MyEventsList { get; set; }
 
         public ViewModelUserPage()
         {
-
             SikonSingleton = Singleton.Instance;
 
-            List<AccountToEvent> liste = new AccountToEventHandler().Read();
+            UpdateUserInfoCommand = new RelayCommand(UpdateUserInfo);
 
-            MyEventsList = new ObservableCollection<AccountToEvent>();
-            foreach (var g in liste.Where(g => SikonSingleton.LoggedAccount.Email == g.Account_ID))
+            Name = SikonSingleton.LoggedAccount.Name;
+            Email = SikonSingleton.LoggedAccount.Email;
+            Password = SikonSingleton.LoggedAccount.Password;
+
+
+            List<AccountToEvent> AccountToEventListe = new AccountToEventHandler().Read();
+
+            List<Event> EventListe = new EventsHandler().Read();
+
+            MyEventsList = new ObservableCollection<Event>();
+
+            List<AccountToEvent> TilmeldteEvents = new List<AccountToEvent>();
+
+            foreach (var g in AccountToEventListe.Where(g => SikonSingleton.LoggedAccount.Email == g.Account_ID))
             {
-
-                MyEventsList.Add(g);
-
+                TilmeldteEvents.Add(g);
+                    
             }
 
+            foreach (var w in TilmeldteEvents)
+            {
+                foreach (var r in EventListe)
+                {
+                    if (w.Event_ID == r.ID)
+                    {
+                        MyEventsList.Add(r);
+                    }
+                }
+            }
 
         }
 
+        public void UpdateUserInfo()
+        {
+            SikonSingleton.LoggedAccount.Name = Name;
+           
+            SikonSingleton.LoggedAccount.Password = Password;
+            new AccountHandler().Update(SikonSingleton.LoggedAccount, SikonSingleton.LoggedAccount.Email);
+        }
     }
 
 }
