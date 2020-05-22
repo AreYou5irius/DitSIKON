0eb96c00a6d78bbff4b8449de75f0a8b74eed91d
Stefan de Haas Hansen <stef9239@edu.easj.dk>
2020-05-22 16:29:11 +0200

    DropDown til at vælge rum under opretkursus

--- SIKONClient/View/OpretKursus.xaml.cs
+++ SIKONClient/View/OpretKursus.xaml.cs
@@ -26,5 +26,10 @@ namespace SIKONClient.View
         {
             this.InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
