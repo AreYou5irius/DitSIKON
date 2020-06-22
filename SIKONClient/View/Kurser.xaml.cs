d41fe299013d5fbb34c735659134d394af9aac13
Christian Allingham <chri793r@edu.easj.dk>
2020-06-23 01:26:53 +0200

    bla

--- SIKONClient/View/Kurser.xaml.cs
+++ SIKONClient/View/Kurser.xaml.cs
@@ -1,17 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
-using Windows.UI.Xaml;
+﻿using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
 using SIKONClient.Model;
 using SIKONClient.View;
 
@@ -54,7 +42,6 @@ namespace SIKONClient
         private void Button_Click_1(object sender, RoutedEventArgs e)
         {
             Frame.Navigate(typeof(OpretKursus));
-            
         }
     }
 }
